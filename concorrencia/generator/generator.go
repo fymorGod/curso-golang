4e23a2ea2b7077f5a0994482c64bf8b88660fee8
fymorGod <mufinagamer@gmail.com>
2023-05-04 15:21:21 -0300

    feat: complete package concorrencia :zap:

---
+++ concorrencia/generator/generator.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"regexp"
+)
+
+// Google I/O 2012 - Go Concurrency Patterns
+func titulo(urls ...string) <-chan string {
+	c := make(chan string)
+	for _, url := range urls {
+		go func(url string) {
+			resp, _ := http.Get(url)
+			html, _ := ioutil.ReadAll(resp.Body)
+
+			r, _ := regexp.Compile("<title>(.*?)<\\/title>")
+			match := r.FindStringSubmatch(string(html))
+			if len(match) > 1 {
+				c <- match[1]
+			} else {
+				c <- "Título não encontrado"
+			}
+		}(url)
+	}
+	return c
+}
+
+func main() {
+	t1 := titulo("https://www.cod3r.com.br", "https://www.google.com")
+	t2 := titulo("https://www.amazon.com.br", "https://www.youtube.com")
+	fmt.Println("Primeiros:", <-t1, "|", <-t2)
+	fmt.Println("Segundos:", <-t1, "|", <-t2)
+}
